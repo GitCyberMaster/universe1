23b020ba6b1f3bebec6fedbe1dd289548f4c1977
GitCyberMaster <wr4teach@gmail.com>
2023-09-27 00:04:47 +0300

    UserTest Add

--- src/test/java/com/fluffy/universe/models/UserTest.java
+++ src/test/java/com/fluffy/universe/models/UserTest.java
@@ -2,7 +2,6 @@ package com.fluffy.universe.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
